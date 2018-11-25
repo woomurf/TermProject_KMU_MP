c6609e482795e2b8662f5f66e3e8758f1d1c26fa
woomurf <dngusdnd@gmail.com>
2018-11-26 03:34:37 +0900

    통계화면 수정 expandable list view 사용

---
+++ 2018MP_TermProject/app/src/main/java/com/kmu/a2018mp_termproject/myGroup.java
@@ -0,0 +1,16 @@
+package com.kmu.a2018mp_termproject;
+
+import java.util.ArrayList;
+
+public class myGroup {
+    public ArrayList<String> child;
+    public String groupName;
+
+    myGroup(String name){
+        groupName = name;
+        child = new ArrayList<String>();
+    }
+
+
+
+}
--- 2018MP_TermProject/app/src/main/java/com/kmu/a2018mp_termproject/statisticsActivity.java
+++ 2018MP_TermProject/app/src/main/java/com/kmu/a2018mp_termproject/statisticsActivity.java
@@ -17,7 +17,9 @@ import com.github.mikephil.charting.highlight.Highlight;
 import java.util.ArrayList;
 import android.graphics.Color;
 import android.util.Log;
+import android.view.Display;
 import android.widget.ArrayAdapter;
+import android.widget.ExpandableListView;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -36,21 +38,26 @@ public class statisticsActivity extends AppCompatActivity {
     Vector<String> categoryList;
     ArrayList<String> xVals;
     ArrayList<Entry> yvalues;
+    Vector<Float> priceRating;
+    Vector<Integer> categoryPrice;
 
-    ListView billList;
-    ArrayAdapter<String> billAdapter;
+    ArrayList<myGroup> billList;
+    ExpandableListView billListView;
+    BaseExpandableAdapter billAdapter;
 
     protected void onCreate(Bundle bundle){
         super.onCreate(bundle);
         setContentView(R.layout.activity_statistics);
+        Display newDisplay = getWindowManager().getDefaultDisplay();
+        int width = newDisplay.getWidth();
+
+
 
         category_db = statisticsActivity.this.openOrCreateDatabase("categoryDB.db",MODE_ENABLE_WRITE_AHEAD_LOGGING,null);
         account_db = statisticsActivity.this.openOrCreateDatabase("AccountBook.db",MODE_ENABLE_WRITE_AHEAD_LOGGING,null);
 
-        billList = (ListView) findViewById(R.id.billList);
-        billAdapter = new ArrayAdapter<String>(statisticsActivity.this,android.R.layout.simple_list_item_1);
-        billList.setAdapter(billAdapter);
-
+        billList = new ArrayList<myGroup>();
+        billListView = (ExpandableListView) findViewById(R.id.BillList);
 
         PieChart pieChart = (PieChart) findViewById(R.id.piechart);
         pieChart.setUsePercentValues(true);
@@ -62,7 +69,11 @@ public class statisticsActivity extends AppCompatActivity {
         xVals = new ArrayList<String>();
 
         getStatistic();
-        getList("","");
+        getListDefault();
+
+        billAdapter = new BaseExpandableAdapter(this,R.layout.group_row,R.layout.child_row,billList);
+        billListView.setIndicatorBounds(width-50,width);
+        billListView.setAdapter(billAdapter);
 
         PieDataSet dataSet = new PieDataSet(yvalues, "Election Results");
 
@@ -118,9 +129,9 @@ public class statisticsActivity extends AppCompatActivity {
             xVals.add(categoryList.elementAt(i));
         }
 
-        Vector<Integer> categoryPrice = new Vector<Integer>();
+        categoryPrice = new Vector<Integer>();
         int totalPrice = getTotalPrice();
-        Vector<Float> priceRating = new Vector<Float>();
+        priceRating = new Vector<Float>();
 
         for(int i = 0; i < categoryList.size(); i++){
             categoryPrice.add(getCategoryPrice(categoryList.elementAt(i)));
@@ -191,35 +202,48 @@ public class statisticsActivity extends AppCompatActivity {
         return price;
     }
 
-    public void getList(String category, String tag) {
+    /*
+    1. list view 에 category 이름(비율%)     총 가격 을 띄워준다.
+    2. 각 list view를 클릭하면 해당 category의 내역을 보여준다.
+    3. category와 tag를 선택하면 해당하는 내역을 다 보여준다.
+    4. list view 위에 총 price를 띄워주는 것이 좋을 것 같다.
+     */
 
-        billAdapter.clear();
+    public void getListDefault() {
 
-        Cursor cursor;
+        for(int i = 0; i < categoryList.size(); i ++){
 
-        if (category != "" && tag != "") {
-            cursor = account_db.rawQuery("SELECT * FROM ACCOUNTBOOK WHERE category = '" + category + "', tag = '" + tag + "'", null);
-        } else if (category == "" && tag == "") {
-            cursor = account_db.rawQuery("SELECT * FROM ACCOUNTBOOK", null);
-        } else if (category == "") {
-            cursor = account_db.rawQuery("SELECT * FROM ACCOUNTBOOK WHERE category = '" + category + "'", null);
-        } else {
-            cursor = account_db.rawQuery("SELECT * FROM ACCOUNTBOOK WHERE tag = '" + tag + "'", null);
+
+            String cRow = "";
+
+            cRow += categoryList.elementAt(i)
+                    +"("
+                    +priceRating.elementAt(i)
+                    +")           "
+                    +categoryPrice.elementAt(i);
+
+            myGroup temp = new myGroup(cRow);
+
+            Cursor cursor;
+            cursor = account_db.rawQuery("SELECT * FROM ACCOUNTBOOK WHERE category = '"+categoryList.elementAt(i)+"'",null );
+
+            while(cursor.moveToNext()){
+                String row = "";
+
+                row += cursor.getString(1)
+                        + " "
+                        + cursor.getInt(2)
+                        + "원";
+
+
+                temp.child.add(row);
+            }
+
+            billList.add(temp);
+
         }
 
 
-        while (cursor.moveToNext()) {
-            String query = "";
-
-            query += cursor.getString(4) + "/ "
-                    + cursor.getString(1) + "/ "
-                    + cursor.getInt(2) + "/ "
-                    + cursor.getString(5);
-
-            billAdapter.add(query);
-        }
-
-        billAdapter.notifyDataSetChanged();
 
 
     }
